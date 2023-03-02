50c5c366f356d27f3cf42dc11ae59a4fbb988ff3
anhquan1301 <nlaquan1301@gmail.com>
2023-03-02 21:56:37 +0700

    update

--- bai-tap/ss3/bai-tap/products/src/main/java/com/example/products/repository/impl/ProductRepository.java
+++ bai-tap/ss3/bai-tap/products/src/main/java/com/example/products/repository/impl/ProductRepository.java
@@ -7,7 +7,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -18,7 +18,7 @@ public class ProductRepository implements IProductRepository {
         List<Product> productList = null;
         try{
             session = SessionUtil.sessionFactory.openSession();
-            productList = session.createQuery("from Product where name =:name").setParameter("name", name).getResultList();
+            productList = session.createQuery("from Product where name like concat('%', :name, '%')").setParameter("name", name).getResultList();
 
         }catch (Exception e){
         }finally {
@@ -31,7 +31,7 @@ public class ProductRepository implements IProductRepository {
 
     @Override
     public Product findById(int id) {
-        return null;
+        return SessionUtil.entityManager.find(Product.class,id);
     }
 
     @Override
@@ -43,7 +43,7 @@ public class ProductRepository implements IProductRepository {
             transaction = session.getTransaction();
             transaction.begin();
             session.persist(product);
-            transaction.commit();
+            transaction.commit();      
         }catch (Exception e){
             if(transaction!=null){
                 transaction.rollback();
@@ -57,11 +57,50 @@ public class ProductRepository implements IProductRepository {
 
     @Override
     public void edit(Product product) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = SessionUtil.sessionFactory.openSession();
+            transaction = session.getTransaction();
+            transaction.begin();
+            Product p = findById(product.getId());
+            p.setName(product.getName());
+            p.setPrice(product.getPrice());
+            p.setStatus(product.getStatus());
+            p.setProducer(product.getProducer());
+            session.update(p);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
+    @Transactional
     public void delete(int id) {
-
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = SessionUtil.sessionFactory.openSession();
+            transaction = session.getTransaction();
+            transaction.begin();
+            Product product = findById(id);
+            session.remove(product);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction!= null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
